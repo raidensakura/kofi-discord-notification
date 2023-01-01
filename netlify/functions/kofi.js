e7f4a21208deff043a4dc235f243b60ee986dcb2
Raiden <raidensakurajima@gmail.com>
2023-01-01 19:55:36 +0800

    🌟

--- netlify/functions/kofi.js
+++ netlify/functions/kofi.js
@@ -4,8 +4,9 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const { Webhook, MessageBuilder } = require('discord-webhook-node');
-const webhook_url = process.env.WEBHOOK_LINK
-const kofi_token = process.env.KOFI_TOKEN
+const webhook_url = process.env.WEBHOOK_URL;
+const kofi_token = process.env.KOFI_TOKEN;
+const kofi_username = process.env.KOFI_USERNAME;
 const webhook = new Webhook(webhook_url);
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,16 +14,16 @@ app.use(bodyParser.json());
 
 app.use('/', async function(req, res) {
 	const data = req.body.data;
-    if (!data) return res.json(`What'chu doin here fam`);
+    if (!data) return res.json(`Hello world.`);
     try {
         const obj = JSON.parse(data);
-		if (obj.verification_token !== kofi_token) return res.json(`Wrong token, fam.`);
+		if (obj.verification_token !== kofi_token) return res.json(`Invalid token provided.`);
         const embed = new MessageBuilder();
 
-		embed.setAuthor('Ko-fi', 'https://cdn.discordapp.com/attachments/1058667662835728394/1058790047924633620/Daco_2133679.png');
-		embed.setThumbnail('https://cdn.discordapp.com/attachments/1058667662835728394/1058790047924633620/Daco_2133679.png')
+		embed.setAuthor('Ko-fi', 'https://i.imgur.com/J0egcX2.png');
+		embed.setThumbnail('https://i.imgur.com/J0egcX2.png');
         embed.setTitle('New supporter on Ko-fi ☕');
-		embed.setURL('https://ko-fi.com/raidensakura');
+		if (!kofi_username) embed.setURL(`https://ko-fi.com/${kofi_username}`);
 
 		switch(obj.tier_name) {
 			case 'Silver':
@@ -39,7 +40,7 @@ app.use('/', async function(req, res) {
 		embed.addField(`Type`, `${obj.type}`, true);
         embed.addField(`Amount`, `${obj.amount} ${obj.currency}`, true);
         if (obj.message && obj.message !== 'null') embed.addField(`Message`, `${obj.message}`);
-		embed.setFooter(`Thank you for supporting us!`, `https://cdn.discordapp.com/emojis/1002579260915060808.webp?size=96&quality=lossless`);
+		embed.setFooter(`Thank you for supporting us!`, `https://i.imgur.com/J0egcX2.png`);
         embed.setTimestamp();
 
         await webhook.send(embed);
